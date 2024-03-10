5b7ff58972b71b339472b7600f8ed702c71700a7
0xmegren <abodimegren@gmail.com>
2024-03-10 22:04:56 +0300

    Initial commit

---
+++ usersapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+
+class Person:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return self.username
+
+# Global list to store instances of Person
+people = []
+
+def add(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        new_person = Person(username, password)
+        people.append(new_person)
+        return redirect("default")
+    return render(request, "add.html")
+
+def default(request):
+    return render(request, "default.html", {"people": people})
+
+    
